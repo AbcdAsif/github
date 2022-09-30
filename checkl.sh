fc2d8fee19d27209060c2c3e08eb0b18b7c32a1a
AbcdAsif <mdasifmm@outlook.com>
2022-09-30 11:54:08 +0000

    First commit

---
+++ checkl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#test by sachin
+#test by Pavithra
+echo "enter the name of a file"
+read name
+#strings
+tested by mallikarjun
+while read line 
+do
+	echo "$line"
+	count=`echo "$line" | wc -c`
+	echo "The Number of characte in line is $count"
+done < $name
